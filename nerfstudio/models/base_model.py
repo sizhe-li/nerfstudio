88e2af556b50a26b66cde2f3f792758e3c3f5912
sizhe-li <sli96@u.rochester.edu>
2023-08-05 01:12:02 -0400

    update

--- nerfstudio/models/base_model.py
+++ nerfstudio/models/base_model.py
@@ -184,7 +184,8 @@ class Model(nn.Module):
                 outputs_lists[output_name].append(output)
         outputs = {}
         for output_name, outputs_list in outputs_lists.items():
-            outputs[output_name] = torch.cat(outputs_list).view(image_height, image_width, -1)  # type: ignore
+            if output_name not in ["plane_tv", "plane_tv_proposal_net"]:
+                outputs[output_name] = torch.cat(outputs_list).view(image_height, image_width, -1)  # type: ignore
         return outputs
 
     def get_outputs_for_camera_ray_bundle_with_grad(self, camera_ray_bundle: RayBundle) -> Dict[str, torch.Tensor]:
